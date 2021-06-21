549838aec514552ba469ac52ad9118b68794d4f0
Garrett Anderson <keaton85@runbox.com>
2021-06-20 21:19:40 -0500

    add event listener to each div

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ container.style.width="800px";
 for (let i=1; i<=16; i++) {    
     for (let j=1; j<=16; j++) {
     let div=document.createElement("div");
+    div.setAttribute("id",`div${i*j}`);
     div.style.height="48px";
     div.style.width="48px";
     div.style.border="1px solid black";
@@ -10,3 +11,9 @@ for (let i=1; i<=16; i++) {
     container.appendChild(div);
 }
 }
+let divs=document.querySelectorAll("div");
+divs.forEach((div) => {
+    div.addEventListener('click',()=>{
+div.style.backgroundColor="blue"
+    })
+})
