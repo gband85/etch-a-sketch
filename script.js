023974169f456411bae2c99ed3a24ae64a85b9ad
Garrett Anderson <keaton85@runbox.com>
2021-06-20 21:02:29 -0500

    fix width of container and divs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let container = document.querySelector(".container");
-
-for (let i=0; i<16; i++) {    
-    for (let j=0; j<16; j++) {
+container.style.width="800px";
+for (let i=1; i<=16; i++) {    
+    for (let j=1; j<=16; j++) {
     let div=document.createElement("div");
-    div.style.height="25px";
-    div.style.width="25px";
+    div.style.height="48px";
+    div.style.width="48px";
     div.style.border="1px solid black";
    // document.createElement("div");
     container.appendChild(div);
