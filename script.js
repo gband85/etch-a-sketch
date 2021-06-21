916990dddbb5133926c141646f0886fecab3af48
Garrett Anderson <keaton85@runbox.com>
2021-06-20 20:14:08 -0500

    fix flex wrap, loop to create divs

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let container = document.querySelector(".container");
 
-for (let i=0; i<16; i++) {
+for (let i=0; i<16; i++) {    
+    for (let j=0; j<16; j++) {
     let div=document.createElement("div");
     div.style.height="25px";
     div.style.width="25px";
     div.style.border="1px solid black";
    // document.createElement("div");
     container.appendChild(div);
+}
 }
