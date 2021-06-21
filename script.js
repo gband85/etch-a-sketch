8971278d303f90a5b8fedae46dd347ed765ad7ed
Garrett Anderson <keaton85@runbox.com>
2021-06-20 19:53:40 -0500

    add dimensions and border to divs

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let container = document.querySelector(".container");
+
 for (let i=0; i<16; i++) {
+    let div=document.createElement("div");
+    div.style.height="25px";
+    div.style.width="25px";
+    div.style.border="1px solid black";
    // document.createElement("div");
-    container.appendChild(document.createElement("div"))
+    container.appendChild(div);
 }
